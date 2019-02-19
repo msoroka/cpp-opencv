2978204b16fa2845fc0410f1860e1bb830a9f3dd
Mateusz Soroka <xmsoroka@gmail.com>
2019-02-19 16:48:48 +0100

    zad01 done

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,40 @@
+#include "/usr/local/Cellar/opencv/4.0.1/include/opencv4/opencv2/highgui.hpp"
+
 #include <iostream>
 
+#define ESC 27
+
+using namespace std;
+using namespace cv;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat image;
+    VideoCapture cap;
+
+    cap.open(0);
+
+    namedWindow("window", WINDOW_AUTOSIZE);
+
+    double dWidth = cap.set(CAP_PROP_FRAME_WIDTH, 640);
+    double dHeight = cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+
+
+    while (true) {
+        cap >> image;
+
+        try {
+            flip(image, image, +1);
+
+            imshow("window", image);
+        } catch (Exception &e) {
+            cout << e.what() << endl;
+
+            return 1;
+        }
+
+        if (waitKey(30) == ESC) break;
+    }
+
+
     return 0;
 }
