3d63ae3b1612e68f2775fd35fc92d08c463a8b7c
Mateusz Soroka <xmsoroka@gmail.com>
2019-03-25 23:39:16 +0100

    Added comments

--- lab4/zad1/main.cpp
+++ lab4/zad1/main.cpp
@@ -14,10 +14,15 @@ using namespace std;
 using namespace cv;
 
 int main() {
+    // Zmienne pomocnicze
     VideoCapture stream;
     Mat image, firstFrame, secondFrame, thirdFrame, result;
+
+    // Zmienne, w k†órych przechowywana jest wartość tych sliderów z opcjami
     int alphaSlider = 1;
     int thresholdSlider = 1;
+
+    // Wybór do menu
     int choice = 0;
 
     cout << "1. Kamerka laptopa" << endl;
@@ -26,16 +31,20 @@ int main() {
     cout << "Podaj żródło obrazu:" << endl;
     cin >> choice;
 
+    // Wybieramy źródło obrazu
     switch (choice) {
         case 1: {
+            // Kamerka internetowa
             stream.open(0);
             break;
         }
         case 2: {
+            // Wideo z dziewczynką na rowerze
             stream.open("/Users/msoroka/Desktop/uczelnia/obrazy/lab4/bike.avi");
             break;
         }
         case 3: {
+            // Wideo z jeżdżącym robotem
             stream.open("/Users/msoroka/Desktop/uczelnia/obrazy/lab4/robot_no_loop.avi");
             break;
         }
@@ -45,15 +54,20 @@ int main() {
         }
     }
 
-
+    // Sprawdzenie czy poprawnie załadowano źródło obrazu
     if (!stream.isOpened()) {
         cout << "Błąd podczas otwierania kamery." << endl;
     } else {
         cout << "Pomyślnie otworzono źródło obrazu." << endl;
     }
 
+    // Złapanie pierwszej klatki obrazu, do dalszych porównań
     stream >> firstFrame;
+
+    // Zrobienie kopii pierwszej klatki 
     secondFrame = firstFrame;
+
+    // Nałożenie szarej barwy na zapisane klatki
     cvtColor(firstFrame, firstFrame, COLOR_BGR2GRAY);
     cvtColor(secondFrame, secondFrame, COLOR_BGR2GRAY);
 
@@ -62,24 +76,38 @@ int main() {
     namedWindow("Camera", WINDOW_AUTOSIZE);
     namedWindow("Settings", WINDOW_AUTOSIZE);
 
+    // Inicjalizacja sliderków z opcjami
     createTrackbar("Alpha", "Settings", &alphaSlider, 300);
     createTrackbar("Threshold", "Settings", &thresholdSlider, 300);
 
     while (true) {
+        // Wczytanie klatki z aktualnym obrazem
         stream >> secondFrame;
         stream >> thirdFrame;
 
+        // Sprawdzenie czy klatki nie są puste, żeby nie wysypało błędu
         if (secondFrame.empty() || thirdFrame.empty()) {
+
+            // Ustawienie obrazu z powrotem na pierwszą klatkę, aby zapętlić
+            // odtwarzanie obrazu
+            // !! Nie wpływa na obraz z kamerki wideo
             stream.set(CAP_PROP_POS_FRAMES, 0);
+
+            // Ponowne wczytanie klatek
             stream >> secondFrame;
             stream >> thirdFrame;
         }
 
+        // Nałożnie barwy szarości
         cvtColor(secondFrame, secondFrame, COLOR_BGR2GRAY);
         cvtColor(thirdFrame, thirdFrame, COLOR_BGR2GRAY);
+
+        // Obliczenie różnicy pomiędzy wczytanymi klatkami
         absdiff(secondFrame, thirdFrame, result);
         threshold(result, result, thresholdSlider, 255, THRESH_BINARY);
 
+        // Obliczenia współczynnika alfa z wzoru podanego na slajdach
+        // i zastosowanie go na obrazie
         double alpha = (double) alphaSlider / 100;
         image = (1 - alpha) * image + alpha * result;
 
--- lab4/zad2/main.cpp
+++ lab4/zad2/main.cpp
@@ -20,22 +20,34 @@ int main() {
     Mat image;
     vector<cv::String> fn;
 
+    // Ścieżka, w której znajdują się obrazki z klatkami z laborek drugich
+    // Zapisanie wszystkich obrazów do wektora stringów 
     glob("/Users/msoroka/Desktop/uczelnia/obrazy/lab4/frames_vid/*.jpg", fn, false);
 
     vector<Mat> images;
+
+    // Ile obrazów udało się wczytać
     int count = fn.size();
 
+    // Przeiterowanie po wszystkich wczytanych obrazkach
+    // i zapisanie ich do wektora obiektów Mat
     for (int i = 0; i < count; i++)
         images.push_back(imread(fn[i]));
 
+    // Pobranie wysokości i szerokości obrazków
     int width = images[0].cols;
     int height = images[0].rows;
 
+    // Utworzenie obiektu klasy Size
     Size frameSize(width, height);
 
+    // Zmienna, do której będzie zapisywane wideo
     VideoWriter outputVideo;
+
+    // Ustawienie nazwy wideo, kodeka, ilości klatek na sekundę i rozmiaru (z klasy Size)
     outputVideo.open("out.mov", -1, 30, frameSize, true);
 
+    // Zapisanie do pliku wideo wczytanych zdjęć
     for (int i = 0; i < count; i++) {
         outputVideo.write(images[i]);
 
