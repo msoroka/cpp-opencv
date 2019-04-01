580a9b9f2a7283b6a35fc88065c32b3444e2c567
Mateusz Soroka <xmsoroka@gmail.com>
2019-04-01 17:56:29 +0200

    lab5 done

--- lab5/zad1/main.cpp
+++ lab5/zad1/main.cpp
@@ -21,10 +21,9 @@ int main() {
 
     // Zmienne pomocnicze
     Mat srcGray, sobelXBin, sobelYBin, gradient, angle;
-    float edge, angleVal;
+    float angleVal;
 
     int thresholdSlider = 20;
-    int thresholdBinarySlider = 1;
     int cannyLowSlider = 50;
     int cannyHighSlider = 150;
 
@@ -40,7 +39,6 @@ int main() {
     namedWindow("Settings", WINDOW_AUTOSIZE);
 
     createTrackbar("Threshold", "Settings", &thresholdSlider, 100);
-    createTrackbar("Threshold Binary", "Settings", &thresholdBinarySlider, 10);
     createTrackbar("Canny Low", "Settings", &cannyLowSlider, 100);
     createTrackbar("Canny High", "Settings", &cannyHighSlider, 200);
 
@@ -56,47 +54,39 @@ int main() {
         Canny(gauss, canny, cannyLowSlider, cannyHighSlider, 3);
 
         // Krawędź pozioma
-        Sobel(gauss, sobelX, -1, 1.0, 0.0, 3);
+        Sobel(gauss, sobelX, CV_32F, 1.0, 0.0, 3);
         threshold(sobelX, sobelXBin, thresholdSlider, 255, THRESH_BINARY);
 
         // Krawędź pionowa
-        Sobel(gauss, sobelY, -1, 0.0, 1.0, 3);
+        Sobel(gauss, sobelY, CV_32F, 0.0, 1.0, 3);
         threshold(sobelY, sobelYBin, thresholdSlider, 255, THRESH_BINARY);
 
-        // Gradient binarny
-        sobelX.convertTo(sobelX, CV_32F, 1.0 / 255);
-        sobelY.convertTo(sobelY, CV_32F, 1.0 / 255);
 
         cartToPolar(sobelX, sobelY, gradient, angle, true);
-        threshold(gradient, gradientBinary, 0.2, 50, THRESH_BINARY);
+        threshold(gradient, gradientBinary, thresholdSlider, 255, THRESH_BINARY);
 
         // Gradient kolorowy
         gradientColoured = gradientBinary.clone();
         cvtColor(gradientColoured, gradientColoured, COLOR_GRAY2BGR);
         gradientColoured.convertTo(gradientColoured, CV_8UC3, 255);
 
-        for (int i = 0; i < gradientBinary.rows; i++) {
-            for (int j = 0; j < gradientBinary.cols; j++) {
-                edge = gradientColoured.at<float>(i, j);
-                Vec3b color;
-
+        for (int i = 0; i < gradient.rows; i++) {
+            for (int j = 0; j < gradient.cols; j++) {
                 angleVal = angle.at<float>(i, j);
+                Vec3b *color = &gradientColoured.at<Vec3b>(i, j);
 
-                if (edge > 0) {
-
+                if (gradientBinary.at<float>(i, j) > 0) {
                     if (angleVal > 45 && angleVal <= 135)
-                        color = Vec3b(255, 255, 255);
+                        *color = Vec3b(255, 255, 255);
                     if (angleVal > 135 && angleVal <= 255)
-                        color = Vec3b(255, 0, 0);
+                        *color = Vec3b(255, 0, 0);
                     if (angleVal > 255 && angleVal <= 315)
-                        color = Vec3b(0, 255, 0);
+                        *color = Vec3b(0, 255, 0);
                     if ((angleVal > 315 && angleVal <= 360) || (angleVal > 0 && angleVal <= 45))
-                        color = Vec3b(0, 0, 255);
+                        *color = Vec3b(0, 0, 255);
                 } else {
-                    color = Vec3b(0, 0, 0);
+                    *color = Vec3b(0, 0, 0);
                 }
-
-                gradientColoured.at<Vec3b>(i, j) = color;
             }
         }
 
